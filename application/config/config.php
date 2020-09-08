7914b5abd2d11c4e298b1bfec6094c73b6801363
Setiawanhada <setiawanhada46@gmail.com>
2020-09-08 10:40:05 +0700

    edit sidebar menu

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci_template_adminlt2/';
+$config['base_url'] = 'http://localhost/konveksi_pos/';
 
 /*
 |--------------------------------------------------------------------------
